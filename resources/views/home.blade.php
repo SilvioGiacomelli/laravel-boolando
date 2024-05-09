bceb7e388be0ab176ddd318d37be28f730c432d5
SilvioGiacomelli <91.giacomelli.silvio@gmail.com>
2024-05-09 17:34:11 +0200

    [Push]

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,20 @@
 @extends('layout.main')
 
 @section('content')
-    <h1>home</h1>
+    <div class="container">
+        <div class="card">
+            <div class="box">
+                <img class="show" src="/img/1.webp" v-show="!isHovering">
+                <img class="hide" src="/img/1b.webp" v-show="isHovering">
+                <div class="cuore">
+                    <h4>&#9829;</h4>
+                </div>
+                <div key="sconto" class="sconto.type"> sconto . value
+                </div>
+            </div>
+            <p class="marca"> product . brand </p>
+            <p class="stile"> product . name </p>
+            <p class="prezzo"> product . price </p>
+        </div>
+    </div>
 @endsection
